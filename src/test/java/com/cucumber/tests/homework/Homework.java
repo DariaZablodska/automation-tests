ce21cb562c1a5b735714ef6875c50ab85a70f720
dzablodska <daria.zablodska@albelli.com>
2017-12-19 12:44:32 +0100

    Project cleanup

--- src/test/java/com/cucumber/tests/homework/Homework.java
+++ src/test/java/com/cucumber/tests/homework/Homework.java
@@ -2,7 +2,12 @@ package com.cucumber.tests.homework;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.Assert.assertTrue;
 
 public class Homework extends BaseSteps {
 
@@ -18,8 +23,23 @@ public class Homework extends BaseSteps {
 
     @Given("^the Bonusprint home page is open$")
     public void bp_Homepage_is_open() {
-        driver.get("https://t-dtap.bonusprint.co.uk/");
-        //wait.until(ExpectedConditions.elementToBeClickable(By.name("firstname")));
+        driver.get("https://www.bonusprint.co.uk/");
+    }
+
+
+    @And("^I click on the My Account link$")
+    public void bp_MyAccount_is_open() {
+
+        WebElement MyAccount = driver.findElement(By.cssSelector("[data-title='My account']"));
+        MyAccount.click();
+    }
+
+    @And("^I click on the Saved projects link$")
+    public void bp_SavedProject_is_open() {
+        WebElement SavedProject = driver.findElement(By.cssSelector("[data-tam='SecondSavedProjects']"));
+        SavedProject.click();
+        assertTrue(driver.getTitle().contains("Login"));
     }
 
 }
+
